121773f2117a0cc2f7a9e5b8a4fc56e98dc0777b
Rafael Carneiro <rafaelsabicarneiro@gmail.com>
2022-08-17 17:58:14 -0300

    calendario implmentado

--- app/Http/Livewire/Recorrentes/RecorrentesCreate.php
+++ app/Http/Livewire/Recorrentes/RecorrentesCreate.php
@@ -31,110 +31,11 @@ class RecorrentesCreate extends Component
 
 
 
-    public function create() //abrir modal para cadastro ----
-    {
-        $this->resetInputFields();
-        $this->openModal();
-    }
-
-    public function edit($id)
-    {
-        $recorrentes = Recorrentes::find($id);
-
-        $this->recorrentes_id = $id;
-
-        $this->categoria = $recorrentes->categoria;
-        $this->descricao = $recorrentes->descricao;
-        $this->value = $recorrentes->value;
-        $this->fonte = $recorrentes->fonte;
-        $this->observacao = $recorrentes->observacao;
-        $this->taxa = $recorrentes->taxa;
-        $this->data = $recorrentes->data;
-
-        $this->openModal();
-    }
-
-
-    public function openModal()
-    {
-        $this->isOpen = true;
-    }
-
-
-    public function closeModal()
-    {
-        $this->isOpen = false;
-    }
-
-    private function resetInputFields()
-    {
-
-        $this->reset();
-    }
-
-
-    public function store()  //PROCESSAMENTO do cadastrar ou editar: updateOrCreate
-
-    {
-
-        //VALOR//VALOR//VALOR//VALOR//VALOR//VALOR//VALOR//VALOR//VALOR
-        $valor_tratado = str_replace('.', '', $this->value);
-        $valor_tratado = str_replace(',', '.', $valor_tratado);
-        $valor_tratado = str_replace('R$', '', $valor_tratado);
-        $valor_tratado = str_replace(' ', '', $valor_tratado);
-
-
-        //TAXA
-        $taxa_tratada = null;
-        if ($this->taxa) {
-            $taxa_tratada = str_replace('.', '', $this->taxa);
-            $taxa_tratada = str_replace(',', '.', $taxa_tratada);
-            $taxa_tratada = str_replace('R$', '', $taxa_tratada);
-            $taxa_tratada = str_replace(' ', '', $taxa_tratada);
-        }
-
-        //EDIÇÃO OU CRIAÇÃO updateOrCreate
-
-        Recorrentes::updateOrCreate(['id' => $this->recorrentes_id], [
-            'descricao' => $this->descricao,
-            'value' => $valor_tratado,
-            'fonte' => $this->fonte,
-            'observacao' => $this->observacao,
-            'taxa' => $taxa_tratada,
-            'data' => $this->data,
-
-        ]);
-
-        $this->resetInputFields();
-
-        session()->flash('message', 'Item registrado com sucesso.');
-
-        $this->closeModal();
-
-    }
-
-
-    public $confirmingItemDeletion = false;
-
-    public function confirmingItemDeletion($id)
-    {
-        $this->confirmingItemDeletion = $id;
-    }
-
-    public function destroy($id)
-    {
-
-        Recorrentes::find($id)->delete();
-        $this->confirmingItemDeletion = false;
-        session()->flash('message', 'Item deletado com sucesso.');
-    }
-
 
     public function render()
     {
-        $searchTerm = '%'.$this->searchTerm.'%';
-        return view('livewire.recorrentes.recorrentes-create',[
 
-        'recorrentes_retorno' => Recorrentes::where('descricao','like', $searchTerm)->orderBy('data', 'desc')->paginate(5)]);
+        return view('livewire.recorrentes.recorrentes-create');
+
     }
 }

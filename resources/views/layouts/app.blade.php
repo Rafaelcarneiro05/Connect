3682590d0fcd9a8535ab6eaab8ea57af247c84e7
henriquebotelhogomes <hbg1119studio1!>
2022-08-29 16:58:19 -0300

    Remoção de vários erros no CONSOLE por causa do AlpineJs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,16 +10,12 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <script defer src="{{ asset('/js/app.js') }}"></script>
+
         <!-- Styles -->
-        <script src="{{ mix('/js/app.js') }}"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.2/main.min.css">
         @livewireStyles
-
-        <!-- Scripts -->
-
-        <script src="{{ mix('js/app.js') }}" defer></script>
-
 
 
         <!-- JQUERY -->
@@ -62,5 +58,7 @@
 
         @livewireScripts
         @stack('script')
+
     </body>
+
 </html>
